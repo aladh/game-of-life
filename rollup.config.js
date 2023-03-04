5b5b1f0975eefb494d3e63563430941d69b717c5
Alishan Ladhani <8869764+aladh@users.noreply.github.com>
2023-03-04 12:26:13 -0500

    Let CSS be inline
    Extraction requires an extra plugin and is not worth it.

--- rollup.config.js
+++ rollup.config.js
@@ -19,12 +19,7 @@ export default {
 	plugins: [
 		svelte({
 			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
+			dev: !production
 		}),
 
 		// If you have external dependencies installed from
